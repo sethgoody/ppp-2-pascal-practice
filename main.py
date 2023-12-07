121b8f63c27bf5119879d4788ca636f07fba7b74
JavaScriptGoggins <sethgoodman657@gmail.com>
2023-12-06 17:57:36 -0800

    completed assignment

---
+++ main.py
@@ -0,0 +1,55 @@
+# first function
+
+# def max_num(a, b, c):
+
+#     maximum = max(a, b, c)
+
+#     return(maximum)
+
+# print (max_num(232, 960, 230))
+
+# second function
+
+# def mult_list(numbers):
+
+#     product = 1
+
+#     for num in numbers:
+#         product *= num
+    
+#     return product
+
+# print (mult_list([5, 5, 6]))
+
+# third function
+
+# def rev_string(string):
+
+#     reverse = string[::-1]
+
+#     return(reverse)
+
+# print(rev_string('Seth is writing python!'))
+
+# fourth function
+
+# def num_within(start, x, end):
+
+#     return start <= x <= end
+
+# print(num_within(20, 23, 25))
+
+
+# pascals function
+
+# def pascal(n):
+
+#     for row in range(n):
+#         number = 13
+#         numbers = []
+#         for col in range(row + 1):
+#             numbers.append(number)
+#             number = number * (row - col) // (col + 1)
+#         print(" ".join(map(str, numbers)))
+
+# pascal(20)
